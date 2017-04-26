1ddb5ae1e664f67ba759010d3fdf6c3ae6b88070
ajszl <ajszl@DESKTOP-6BCJL1R>
2017-04-26 13:26:08 +0900

    finish

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *     _PersonTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 :2017. 4. 26.
+ *</pre>
+ *
+ * @author : ajszl
+ * @version : 1.0
+ */
+public class PersonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		p.setName("Tom");
+		p.getName();
+		System.out.println(p);
+		Korean k = new Korean("홍길동");
+		System.out.println(k);
+		Japanese j = new Japanese("다나까");
+		System.out.println(j);
+		Chinese c = new Chinese("왕밍");
+		System.out.println(c);
+		System.out.println();
+		p.sayHello();
+		k.sayHello();
+		j.sayHello();
+		c.sayHello();
+		System.out.println();
+		p.sayBye();
+		k.sayBye();
+		j.sayBye();
+		c.sayBye();
+		
+
+	}
+
+}
